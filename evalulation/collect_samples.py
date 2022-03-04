c25e21233a2ca463b4c4dcd0d73027c353830946
Peter Zhang <mbyzhang@outlook.com>
2022-03-04 18:22:36 +0000

    evaluation: move common dataclasses to `common.py`

--- evalulation/collect_samples.py
+++ evalulation/collect_samples.py
@@ -1,7 +1,6 @@
 from typing import List
-from dataclasses import dataclass, field
-from dataclasses_json import dataclass_json, config
 from tqdm import tqdm
+from common import ExperimentManifest, ExperimentParams, ExperimentRun
 
 import argparse
 import os
@@ -14,36 +13,6 @@ import soundfile
 import itertools
 import base64
 import numpy as np
-
-@dataclass_json
-@dataclass
-class ExperimentRun:
-    tx_platform: str = "Unspecified Tx Platform"
-    tx_baudrate: float = 100.0
-    tx_modulation: str = "fsk"
-    tx_modulation_freqs: List[float] = field(default_factory=lambda: [3200.0, 3000.0]) # ignored if modulation!=fsk
-    tx_mode: str = "message"
-    tx_payload: bytes = field(default_factory=bytes, metadata=config(
-        encoder=lambda o: base64.b64encode(o).decode(),
-        decoder=base64.b64decode
-    )) # ignored if mode=chirp/audiofile/alternating
-    distance_m: float = 0.0
-    uuid: str = ""
-
-@dataclass_json
-@dataclass
-class ExperimentParams:
-    name: str = "Untitled"
-    recording_warmup_duration_s: float = 1.0
-    recording_cooldown_duration_s: float = 1.0
-    experiment_run_cooldown_duration_s: float = 5.0
-    recording_fs: int = 48000
-
-@dataclass_json
-@dataclass
-class ExperimentManifest:
-    runs: List[ExperimentRun] = field(default_factory=list)
-    params: ExperimentParams = field(default_factory=ExperimentParams)
 
 def get_platform_string():
     return platform.node() + ": " + platform.platform()
