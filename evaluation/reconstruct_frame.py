d7d8bb8d3c46294130ae6efd70833bec4de7cb7b
Peter Zhang <mbyzhang@outlook.com>
2022-04-29 00:30:03 +0800

    evaluation: reconstruct frames: separate demodulation

--- evaluation/reconstruct_frame.py
+++ evaluation/reconstruct_frame.py
@@ -13,38 +13,45 @@ sys.path.insert(0, "../psrecv")
 
 from modules.io import SoundFileSource
 
-def demodulate(run: ExperimentRun, base_dir: str = ".", block_size: int = 4096):
+def get_pipeline(run: ExperimentRun, fs: int):
+    sps = fs // run.tx_baudrate
+
+    format = {
+        "message": "standard",
+        "raw": "payload_no_ecc_lc",
+    }[run.tx_mode]
+
+    if run.tx_modulation == "dbpsk":
+        profile = "dbpsk"
+    elif run.tx_modulation == "fsk":
+        if len(run.tx_modulation_freqs) == 2:
+            profile = "bfsk"
+        else:
+            profile = "mfsk"
+    else:
+        raise ValueError("Unsupport modulation")
+
+    pipeline = importlib.import_module("." + profile, "profiles").get_pipeline(
+        fs=fs,
+        sps=sps,
+        carrier_freqs=run.tx_modulation_freqs,
+        carrier_f_delta=100,
+        frame_format=format,
+    )
+
+    return pipeline
+
+def get_source(run: ExperimentRun, base_dir: str, block_size: int = 4096):
     filename = os.path.join(base_dir, get_base_filename(EXPERIMENT_RUN_BASE_FILENAME_FORMAT, run) + ".wav")
     source = SoundFileSource(filename, block_size=block_size)
+    return source
 
+def demodulate(run: ExperimentRun, base_dir: str = ".", block_size: int = 4096):
+    source = get_source(run, base_dir, block_size)
     frames = []
 
     with source:
-        fs = source.fs
-        sps = fs // run.tx_baudrate
-
-        format = {
-            "message": "standard",
-            "raw": "payload_no_ecc_lc",
-        }[run.tx_mode]
-
-        if run.tx_modulation == "dbpsk":
-            profile = "dbpsk"
-        elif run.tx_modulation == "fsk":
-            if len(run.tx_modulation_freqs) == 2:
-                profile = "bfsk"
-            else:
-                profile = "mfsk"
-        else:
-            raise ValueError("Unsupport modulation")
-
-        pipeline = importlib.import_module("." + profile, "profiles").get_pipeline(
-            fs=fs,
-            sps=sps,
-            carrier_freqs=run.tx_modulation_freqs,
-            carrier_f_delta=100,
-            frame_format=format,
-        )
+        pipeline = get_pipeline(run, source.fs)
 
         for block in source.stream:
             block_frames = pipeline(block)
